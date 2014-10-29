d7c8d3f18d57b5c46a5a3071d2ca5cc70a048ed1
Riley Schuit <riley.schuit@gmail.com>
2014-10-28 23:20:54 -0700

    modified:   routes.js 	modified:   views/AdminProfileModify.html 	modified:   views/AdminUserCreate.html 	modified:   views/Index.html 	deleted:    views/UserList.html 	modified:   views/UserProfileModify.html

--- routes.js
+++ routes.js
@@ -2,7 +2,42 @@ var crypto = require('crypto');
 var express = require('express');
 module.exports = function(app) {
   var users = require('./controllers/users_controller');
+  //
+  // Public
+  //
   app.use('/public', express.static( './public'));
+  //
+  // Admin pages
+  //
+  app.get('/admin/profile/modify/:id/json', users.UserModifyJSON);
+  app.get('/admin/profile/modify/:id', function(req, res){
+    if (req.session.username !== 'admin') {
+      res.redirect('/');
+      req.session.msg = 'Access denied';
+    } else {
+      res.render('AdminProfileModify');
+    }
+  });
+  app.get('/admin/user/create', function(req, res){
+    if(req.session.username !== 'admin'){
+      res.redirect('/');
+      req.session.msg = 'Access denied!';
+    }
+    res.render('AdminUserCreate', {msg:req.session.msg});
+  });
+  app.post('/admin/user/create', users.UserCreate);
+  app.get('/admin/user/list/json', users.UserListJSON);
+  app.get('/admin/user/list', function(req, res){
+    if(req.session.username !== 'admin'){
+      res.redirect('/');
+      req.session.msg = 'Access denied!';
+    }
+    res.render('AdminUserList', {username: req.session.username});
+  });
+  app.post('/admin/user/:id/delete', users.UserDelete);
+  //
+  // User Pages
+  //
   app.get('/', function(req, res){
     if (req.session.user) {
       res.render('Index', {username: req.session.username,
@@ -13,6 +48,7 @@ module.exports = function(app) {
       res.redirect('/user/login');
     }
   });
+  app.get('/user/profile/json', users.getUserProfile);
   app.get('/user/profile/modify', function(req, res){
     if (req.session.user) {
       res.render('UserProfileModify', {msg:req.session.msg});
@@ -21,45 +57,17 @@ module.exports = function(app) {
       res.redirect('/user/login');
     }
   });
-  app.get('/user/create', function(req, res){
-    if(req.session.username !== 'admin'){
-      res.redirect('/');
-      req.session.msg = 'Access denied!';
-    }
-    res.render('AdminUserCreate', {msg:req.session.msg});
-  });
+  app.post('/user/profile/update', users.UserUpdate);
   app.get('/user/login',  function(req, res){
     if(req.session.user){
       res.redirect('/');
     }
     res.render('UserLogin', {msg:req.session.msg});
   });
+  app.post('/user/login', users.UserLogin);
   app.get('/user/logout', function(req, res){
     req.session.destroy(function(){
       res.redirect('/user/login');
     });
-  });
-  // Admin - modify user
-  app.get('/admin/modify/:id/json', users.UserModifyJSON);
-  app.get('/admin/modify/:id', function(req, res){
-    if (req.session.user !== admin) {
-      res.redirect('/');
-      req.session.msg = 'Access denied';
-    } else {
-      res.render('AdminProfileModify');
-    }
-  });
-  app.post('/admin/create', users.UserCreate);
-  app.post('/user/update', users.UserUpdate);
-  app.post('/user/delete', users.UserDelete);
-  app.post('/user/login', users.UserLogin);
-  app.get('/user/profile', users.getUserProfile);
-  app.get('/user/list/json', users.UserListJSON);
-  app.get('/user/list', function(req, res){
-    if(req.session.username !== 'admin'){
-      res.redirect('/');
-      req.session.msg = 'Access denied!';
-    }
-    res.render('UserList', {username: req.session.username});
   });
 };
