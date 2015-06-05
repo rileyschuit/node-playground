e02dea632ba737c9ad30ef10db128d33f04ea14f
Riley Schuit <rileyschuit@mail.rileyschuit.com>
2015-06-05 15:28:54 -0700

    Corrected update pages

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ var app = express();
 var expressSession = require('express-session');
 var mongoStore = require('connect-mongo')(express);
 var mongoose = require('mongoose');
-require('./models/users_model.js');
 var config = require('./configuration.json');
 
-// Seeding for development:
-require('./models/user-seed.js');
+// Models
+require('./server/models/users_model.js');
 
+// Seeding for development, TODO:  Mange this with grunt or gulp:
+require('./server/models/user-seed.js');
+
+//
 app.configure(function () {
   app.set('ip', process.env.IP || config.site.ip);
   app.set('port', process.env.PORT || config.site.port);
-  app.set('views', path.join(__dirname, 'views'));
+  //app.set('views', path.join(__dirname, 'views'));
   //temp for fix:
   app.engine('.html', require('ejs').__express);
-  //app.set('view engine', 'jade');
+  //app.set('view engine', 'ejs')
   app.set('view engine', 'html');
   app.set('view options', {layout: false});
    app.use(express.urlencoded());
@@ -33,19 +36,13 @@ app.configure(function () {
 });
 
 //Connect to mongodb, refer to configuration file
-var dbUrl = 'mongodb://';
-dbUrl += config.db.username + ':' + config.db.password + '@' + config.db.host + ':' + config.db.port + '/' + config.db.db
+var dbUrl = 'mongodb://' + config.db.username + ':' + config.db.password + '@' + config.db.host + ':' + config.db.port + '/' + config.db.db
 mongoose.connect(dbUrl);
 mongoose.connection.on('open', function () {
 });
 
 // routes
 require('./routes')(app);
-
-//TODO:  This doesn't work right, trying to check admin acount with user controller method
-// Initall Application startup, check if admin exists, if not, create with admin as the password
-var init = require('./controllers/users_controller.js').Initilization;
-init;
 
 // Uses site.ip, site.port to setup destination IP:Port
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
--- routes.js
+++ routes.js
@@ -1,14 +1,53 @@
 var crypto = require('crypto');
 var express = require('express');
 module.exports = function(app) {
-  var users = require('./controllers/users_controller');
+  var users = require('./server/controllers/users_controller');
   //
   // Public TODO:  Start using bower
   //
   app.use('/public', express.static( './public'));
+  //app.use('/', express.static( './client'));
+  //
+  app.get('/user/login',  function(req, res){
+    if(req.session.user){
+      res.redirect('/');
+    }
+    res.render('UserLogin', {username: req.session.username, msg:req.session.msg, title: 'Playgound'});
+  });
+  app.post('/user/login', users.UserLogin);
+  //
+  app.get('/user/logout', function(req, res){
+    req.session.destroy(function(){
+      res.redirect('/user/login');
+    });
+  });
   //
   // Admin pages
   //
+  // Creation:
+  //
+  app.get('/admin/user/create', function(req, res) {
+    if(req.session.username !== 'admin') { 
+      res.redirect('/');
+      req.session.msg = 'Access denied!  You must be an Admin user to create an account';
+    }
+    res.render('AdminUserCreate', {username: req.session.username, msg:req.session.msg, title: 'Playgound'});
+  });
+  app.post('/admin/user/create', users.UserCreate);
+  //
+  // Deletion:
+  //
+  app.get('/admin/user/delete/:id', users.UserDelete);
+  //
+  // Administrative User management:
+  //
+  app.get('/admin/user/list', function(req, res){
+    if(req.session.username !== 'admin'){
+      res.redirect('/');
+      req.session.msg = 'Access denied! Only admin can access /admin/user/list';
+    }
+    res.render('AdminUserList', {username: req.session.username, title: 'Playgound'});
+  });
   app.get('/admin/profile/modify/:id', function(req, res){
     if (req.session.username !== 'admin') {
       res.redirect('/');
@@ -17,27 +56,8 @@ module.exports = function(app) {
       res.render('AdminProfileModify', { title: 'Playgound' });
     }
   });
-  app.put('/admin/profile/update', users.AdminUserUpdate); 
-  app.get('/admin/user/create', function(req, res) {
-    
-    //res.render('AdminUserCreate', {username: req.session.username, msg:req.session.msg});
-    if(req.session.username !== 'admin') { 
-      res.redirect('/');
-      req.session.msg = 'Access denied!  You must be an Admin user to create an account';
-    }
-    res.render('AdminUserCreate', {username: req.session.username, msg:req.session.msg, title: 'Playgound'});
-  });
-  app.post('/admin/user/create', users.UserCreate);
-  app.get('/admin/user/list', function(req, res){
-    if(req.session.username !== 'admin'){
-      res.redirect('/');
-      req.session.msg = 'Access denied!';
-    }
-    res.render('AdminUserList', {username: req.session.username, title: 'Playgound'});
-  });
-  app.get('/admin/user/delete/:id', users.UserDelete);
   //
-  // User Pages
+  // User Self Account Management:
   //
   app.get('/', function(req, res){
     if (req.session.user) {
@@ -55,26 +75,9 @@ module.exports = function(app) {
       res.redirect('/user/login');
     }
   });
-  app.put('/user/profile/update', users.UserUpdate);
-  app.get('/user/login',  function(req, res){
-    if(req.session.user){
-      res.redirect('/');
-    }
-    res.render('UserLogin', {username: req.session.username, msg:req.session.msg, title: 'Playgound'});
-  });
-  app.post('/user/login', users.UserLogin);
-  app.get('/user/logout', function(req, res){
-    req.session.destroy(function(){
-      res.redirect('/user/login');
-    });
-  });
   //
-  // HTTP error codes
+  // API:
   //
-  //app.get('*', function(req, res){
-  //  res.send('what??? (404): Page not Found', 404);
-  //});
-// TODO:  Resort, make this simpler to read...
   app.get('/admin/user/list/json', users.UserListJSON);  //TODO:  Require admin
   app.get('/admin/user/:id/json', users.getUserJSON);
   app.post('/admin/user/update', users.AdminUserUpdate);
